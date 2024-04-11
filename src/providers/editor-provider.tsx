096f6ff66e54728a8f73d2725453d85a0faeb6c3
Adam Shelley <adam.shelley18@gmail.com>
2024-04-11 21:11:40 +0100

    Implementing ReactFlow for connection building

--- src/providers/editor-provider.tsx
+++ src/providers/editor-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EditorNodeType } from "@/lib/types";
+import { EditorActions, EditorNodeType } from "@/lib/types";
 import {
   Dispatch,
   createContext,
@@ -57,7 +57,109 @@ const initialHistoryState: HistoryState = {
   currentIndex: 0,
 };
 
-const initialSTate: EditorState = {
+const initialState: EditorState = {
   editor: initialEditorState,
   history: initialHistoryState,
 };
+
+const editorReducer = (
+  state: EditorState = initialState,
+  action: EditorActions
+): EditorState => {
+  switch (action.type) {
+    case "REDO":
+      if (state.history.currentIndex < state.history.history.length - 1) {
+        const nextIndex = state.history.currentIndex + 1;
+        const nextEditorState = { ...state.history.history[nextIndex] };
+        const redoState = {
+          ...state,
+          editor: nextEditorState,
+          history: {
+            ...state.history,
+            currentIndex: nextIndex,
+          },
+        };
+        return redoState;
+      }
+      return state;
+
+    case "UNDO":
+      if (state.history.currentIndex > 0) {
+        const prevIndex = state.history.currentIndex - 1;
+        const prevEditorState = { ...state.history.history[prevIndex] };
+        const undoState = {
+          ...state,
+          editor: prevEditorState,
+          history: {
+            ...state.history,
+            currentIndex: prevIndex,
+          },
+        };
+        return undoState;
+      }
+      return state;
+
+    case "LOAD_DATA":
+      return {
+        ...state,
+        editor: {
+          ...state.editor,
+          elements: action.payload.elements || initialEditorState.elements,
+          edges: action.payload.edges,
+        },
+      };
+
+    case "SELECTED_ELEMENT":
+      return {
+        ...state,
+        editor: {
+          ...state.editor,
+          selectedNode: action.payload.element,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export type EditorContextData = {
+  previewMode: boolean;
+  setPreviewMode: (value: boolean) => void;
+};
+
+export const EditorContext = createContext<{
+  state: EditorState;
+  dispatch: Dispatch<EditorActions>;
+}>({
+  state: initialState,
+  dispatch: () => undefined,
+});
+
+type EditorProps = {
+  children: React.ReactNode;
+};
+
+const EditorProvider = (props: EditorProps) => {
+  const [state, dispatch] = useReducer(editorReducer, initialState);
+
+  return (
+    <EditorContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </EditorContext.Provider>
+  );
+};
+
+export const useEditor = () => {
+  const context = useContext(EditorContext);
+  if (!context) {
+    throw new Error("useEditor Hook must be used within the editor Provider");
+  }
+  return context;
+};
+
+export default EditorProvider;
